32ca7eb9fcb5a1371889b6b4b8adef2315114237
Matt Sephton <matt.sephton@gmail.com>
2026-08-07 17:10:16 +0100

    Initial Commit

---
+++ DCGDSD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DCGDSD
+//
+//  Created by Matt Sephton on 2026-08-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DCGDSD/DCGDSDApp.swift
@@ -0,0 +1,17 @@
+//
+//  DCGDSDApp.swift
+//  DCGDSD
+//
+//  Created by Matt Sephton on 2026-08-07.
+//
+
+import SwiftUI
+
+@main
+struct DCGDSDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
